6f7ff26d23537208a4a727845c5e4400344ca111
Levi Milli <107660704+LeviMilli@users.noreply.github.com>
2022-09-14 02:52:07 -0700

    jump works. stop on bottom?

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let ctx = canvas.getContext('2d');
 canvas.style.border = '2px solid black';
 
 // load all images
-let bg = new Image();
+let bg = document.createElement('img')
 bg.src = './images/bg.png';
 
 let fg = new Image();
@@ -20,21 +20,79 @@ pipeSouth.src = './images/pipeSouth.png'
 
 let intervalId = 0;
 let isGameOver = false;
+let score = 0; 
+let birdY = 170, birdX = 30;
+let falling = true;
 
+let pipes = [
+    { x: 110, y: -100},
+    { x: 330, y: -80},
+]
 
-// basic animation template
 function draw(){
+    ctx.drawImage(bg, 0, 0)
+    ctx.drawImage(bird, birdX, birdY)
+    let gap =  pipeNorth.height + 150
+
+    for (let i =0; i<pipes.length; i++ ){
+        ctx.drawImage(pipeNorth, pipes[i].x, pipes[i].y)
+        ctx.drawImage(pipeSouth, pipes[i].x, pipes[i].y + gap)
+        pipes[i].x--
+
+        if (pipes[i].x + pipeNorth.width < 0) {
+            pipes[i] = {
+                x: 400,
+                y: -Math.floor(Math.random() * pipeNorth.height)
+            }
+        }
+        if (pipes[i].x == 0) {
+            score++
+        }
+
+        let birdWithPipeStart = birdX + bird.width >= pipes[i].x 
+        let birdWithPipeEnd =  birdX <= pipes[i].x + pipeNorth.width
+        let birdPipeNorth = birdY <=  pipes[i].y + pipeNorth.height
+        let birdPipeSouth = birdY + bird.height >=  pipes[i].y + gap
+
+        if (( birdWithPipeStart && birdWithPipeEnd ) && 
+            (birdPipeNorth ||birdPipeSouth ) 
+        ) {
+            isGameOver = true;
+        }
+    }
+
+    
+    if (falling) {
+        birdY = birdY + 2;
+    }
+    else {
+        birdY = birdY - 10
+    }
+
+    if (birdY + bird.height > canvas.height - fg.height) {
+        isGameOver = true;
+    }
+    
+    ctx.drawImage(fg, 0, canvas.height - fg.height)
+    ctx.font = '22px Roboto';
+    ctx.fillText(`Score: ${score}`, 20, canvas.height - 50);
 
     if (isGameOver) {
         cancelAnimationFrame(intervalId)
-
     }
     else {
         intervalId = requestAnimationFrame(draw)
     }
-    
 }
 
 window.addEventListener('load', () => {
     draw()
+
+    document.addEventListener('mousedown', () => {
+        falling = false;
+    })
+    document.addEventListener('mouseup', () => {
+        falling = true;
+    })
+
 })
